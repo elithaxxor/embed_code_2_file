e7766719a988dad7808ea20dbcb1ffe2abce2651
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-03-18 17:38:01 -0700

    Update main.py

--- embedd_code_jpg/main.py
+++ embedd_code_jpg/main.py
@@ -1,14 +1,19 @@
 import os, sys, traceback, io, time
 import PIL.Image
+# copyleft material, all wrongs reserved. 
 
-#### IMAGE OFFSET (HEX VAL) == FFD9
+############ IMPORTANT CODE FOR REFACTOR ###############
+##  IMAGE OFFSET (HEX VAL) == FFD9
 ## 'ab' == append bytes
 ## OFFSET = byte_content.index(bytes.fromhex('FFD9')) --> to find hex offset val 
 ## f.seek(OFFSET+2) --> to read hex after .picture offset val. 
 ## inject_bytes = PIL.Image.open(inject_img_bytes) <-- create new file 
 ## byte_array = io.BytesIO() <-- create new file 
 ## inject_bytes.save(byte_array, format='PNG') <-- create new file 
-#############################################
+##     try: [<--- WRITE BINARY TO FILE]
+##        with open(orig_img, 'ab') as f, open(exec_file, 'rb') as e:
+##            f.write(e.read())
+#########################################################
 
 ''' 
     * The code embeds hidden messages into png, and writes binary data to datastructure. The emedded file will execute upon running program, so beware.. 
@@ -136,6 +141,7 @@ def main():
     print('x'*50)
     input_text = input('add the text to embed:')
     print('[+] To Add Code to Images [+]')
+    print('[!] Copyleft material, all wrongs reserved!') 
     print('[+] Processing \n', img_dir)
     text_results = add_text(str(img_dir), str(input_text))
     print('adding text result: ', text_results,'\n',' X' * 50)
